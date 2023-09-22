24b26087c7f384960e5cd017e813bc212090b215
brunopdrocha <brunopdrocha@gmail.com>
2023-09-22 09:48:17 -0300

    Montagem do Tipo Ingresso

--- src/classes/TipoIng.java
+++ src/classes/TipoIng.java
@@ -1,5 +1,36 @@
 package classes;
 
-public class TipoIng extends Ing{
-    
+public class TipoIng {
+    public boolean inteira;
+    public boolean meia;
+
+    public TipoIng(boolean inteira,boolean meia){
+        this.inteira = true;
+        this.meia =false;
+    }
+
+    public boolean isInteira() {
+        return this.inteira;
+    }
+
+    public boolean getInteira() {
+        return this.inteira;
+    }
+
+    public void setInteira(boolean inteira) {
+        this.inteira = inteira;
+    }
+
+    public boolean isMeia() {
+        return this.meia;
+    }
+
+    public boolean getMeia() {
+        return this.meia;
+    }
+
+    public void setMeia(boolean meia) {
+        this.meia = meia;
+    }
+
 }
