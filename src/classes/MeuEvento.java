4d042e56a3cf8f4147b2fde5a742ddd42b04da2e
mncbl <mateusncbl@gmail.com>
2023-10-10 20:48:47 -0300

    colocando as coisas, falta so o case 5

---
+++ src/classes/MeuEvento.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class MeuEvento extends Evento {
+    public MeuEvento(String nome, String data, String local, int ingressoInteira, int ingressoMeia, double precoCheio) {
+        super(nome, data, local, ingressoInteira, ingressoMeia, precoCheio);
+    }
+}
---
+++ src/tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+ntln("nome " + evento.getNome());
+                    System.out.println("data " + evento.getData());
+                    System.out.println("local " + evento.getLocal());
+                    System.out.println("quantidade de ingresso(s) " + qtd);
+
+                    if (evento.getTipoIngresso()== 1) {
+                        System.out.println("Inteira");
+
+                    }else{
+                        System.out.println("Meia");
+                    }
