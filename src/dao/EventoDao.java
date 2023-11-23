7424f9ffde29c2e5815a9950a9cb48ac96236e6c
Thiago <thiagocbrandao2003@gmail.com>
2023-11-23 19:24:34 -0300

    txt adicionado

--- src/dao/EventoDao.java
+++ src/dao/EventoDao.java
@@ -1,5 +1,8 @@
 package dao;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -66,6 +69,22 @@ public class EventoDao {
                 System.out.println(evento);
                 break; // Como o evento foi encontrado, podemos interromper a busca
             }
+        }
+    }
+
+    // Método para salvar eventos em um arquivo
+    public void salvarEventosEmArquivo() {
+        String nomeArquivo = "eventos.txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo, true))) {
+            for (Evento evento : eventos) {
+                writer.write(evento.toString());  // Certifique-se de ter o método toString() implementado nas classes Evento, Exposicao, Show, Jogo
+                writer.newLine();  // Adiciona uma nova linha após cada evento
+            }
+            System.out.println("Eventos salvos em " + nomeArquivo);
+        } catch (IOException e) {
+            System.err.println("Erro ao salvar eventos no arquivo.");
+            e.printStackTrace();
         }
     }
 }
